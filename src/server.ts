c2648da6ea8d66a26a1b5b5301071b3d6afb90ce
unknown <leo.fernandes98@hotmail.com>
2021-06-23 21:49:00 -0300

    Segunda aula

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express, { response } from "express";
+import "reflect-metadata";
+import express from "express";
+
+import { router } from "./routes";
+
+import "./database"
+
+
 const app = express();
-
-
-app.get("/test", (request,response) => {
-  return response.send("olá mundo")
-});
-app.post("/test-post", (request,response) =>{
-  return response.send("olá mundo post")
-});
+app.use(express.json());
+app.use(router); 
 
 app.listen(3000, () => console.log("server is running"));
